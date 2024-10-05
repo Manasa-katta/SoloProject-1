67001cf3c9a9170abdfcf6b6c6bece9538351ab1
Manasa-katta <kattamanasa1911@gmail.com>
2024-10-05 12:57:04 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="https://tse3.mm.bing.net/th?id=OIP.rdDJbj2fgaC3NmjAfF_FlwHaHa&pid=Api&P=0&h=180" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
